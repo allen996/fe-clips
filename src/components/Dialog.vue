c18d08ed161e1fe6aed5a1f45903fd5d23d600f6
allen996 <1964621612@qq.com>
2023-06-15 23:14:51 +0800

    add dialog transition

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -29,74 +29,93 @@ function enterEmail(): void {
 function cancelEmail(): void {
   isShowEmail.value = false
 }
+
+const selected = ref('')
+async function handleSeleted(e: MouseEvent): Promise<void> {
+  const target = e.target as HTMLElement
+
+  if (target.tagName === 'LI') {
+    selected.value = target.textContent!
+  }
+}
 </script>
 
 <template>
   <Teleport to="body">
     <div class="overlay" @click.self="close">
-      <div class="dialog">
-        <header>
-          选择共享资源的方式
-          <i class="iconfont icon-close" @click="close"></i>
-        </header>
-        <div class="content">
-          <div class="way">
-            <button id="email" @click="enterEmail">
-              <i class="iconfont icon-clips-logo1"></i>
-            </button>
-            <span>邮箱</span>
-          </div>
-          <div class="way">
-            <button id="github">
-              <a href="https://github.com/allen996/fe-clips/issues" target="_blank">
-                <i class="iconfont icon-github"></i>
-              </a>
-            </button>
-            <span>github</span>
-          </div>
-        </div>
-      </div>
-      <div class="email" v-if="isShowEmail">
-        <header>请输入您想要分享的资源</header>
-        <form action="">
-          <div class="name">
-            <label for="">链接名</label>
-            <input type="text" />
-          </div>
-          <div class="link">
-            <label for="">链接地址</label>
-            <input type="text" />
-          </div>
-          <div class="description">
-            <label for="">描述</label>
-            <input type="text" />
-          </div>
-          <div class="category">
-            <label for="">类别</label>
-            <div class="select">
-              <div class="selector" @click="handleSelectDropdown">
-                <input type="text" placeholder="请选择链接相应的类别" readonly />
-                <i class="iconfont icon-downarrow1f" :style="rotateObj"></i>
-              </div>
-              <Transition name="dropdown">
-                <ul class="options" v-show="showDropdown">
-                  <li>111</li>
-                  <li>222</li>
-                  <li>333</li>
-                  <li>333</li>
-                  <li>333</li>
-                  <li>333</li>
-                  <li>333</li>
-                </ul>
-              </Transition>
+      <Transition name="dialog">
+        <div class="dialog" v-if="!isShowEmail">
+          <header>
+            选择共享资源的方式
+            <i class="iconfont icon-close" @click="close"></i>
+          </header>
+          <div class="content">
+            <div class="way">
+              <button id="email" @click="enterEmail">
+                <i class="iconfont icon-clips-logo1"></i>
+              </button>
+              <span>邮箱</span>
+            </div>
+            <div class="way">
+              <button id="github">
+                <a href="https://github.com/allen996/fe-clips/issues" target="_blank">
+                  <i class="iconfont icon-github"></i>
+                </a>
+              </button>
+              <span>github</span>
             </div>
           </div>
-        </form>
-        <footer>
-          <button @click="cancelEmail">取消</button>
-          <button>确认</button>
-        </footer>
-      </div>
+        </div>
+      </Transition>
+      <Transition name="email">
+        <div class="email" v-if="isShowEmail">
+          <header>请输入您想要分享的资源</header>
+          <form action="">
+            <div class="name">
+              <label for="">链接名</label>
+              <input type="text" />
+            </div>
+            <div class="link">
+              <label for="">链接地址</label>
+              <input type="text" />
+            </div>
+            <div class="description">
+              <label for="">描述</label>
+              <input type="text" />
+            </div>
+            <div class="category">
+              <label for="">类别</label>
+              <div class="select">
+                <div class="selector" @click="handleSelectDropdown">
+                  <input
+                    type="text"
+                    placeholder="请选择链接相应的类别"
+                    readonly
+                    :value="selected"
+                    id="category"
+                  />
+                  <i class="iconfont icon-downarrow1f" :style="rotateObj"></i>
+                </div>
+                <Transition name="dropdown">
+                  <ul class="options" v-show="showDropdown" @click="handleSeleted">
+                    <li value="111">111</li>
+                    <li value="222">222</li>
+                    <li value="333">333</li>
+                    <li>333</li>
+                    <li>333</li>
+                    <li>333</li>
+                    <li>333</li>
+                  </ul>
+                </Transition>
+              </div>
+            </div>
+          </form>
+          <footer>
+            <button @click="cancelEmail">取消</button>
+            <button>确认</button>
+          </footer>
+        </div>
+      </Transition>
     </div>
   </Teleport>
 </template>
@@ -110,13 +129,31 @@ function cancelEmail(): void {
   left: 0;
   background-color: rgba(0, 0, 0, 0.5);
 
+  .dialog-enter-active,
+  .email-enter-active {
+    animation: scaling 0.5s;
+  }
+  .dialog-leave-active,
+  .email-leave-active {
+    animation: scaling 0.5s reverse;
+  }
+
+  @keyframes scaling {
+    from {
+      transform: scale(0);
+    }
+    to {
+      transform: scale(1);
+    }
+  }
+
   .dialog,
   .email {
     width: 50vw;
+    height: fit-content;
     position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+    inset: 0;
+    margin: auto;
     background-color: var(--fc-background-color-1);
     border-radius: 4px;
     z-index: 9999;
@@ -170,11 +207,12 @@ function cancelEmail(): void {
       }
     }
   }
+
   .email {
     caret-color: var(--fc-color-primary);
 
     form {
-      padding: 20px 50px;
+      padding: 20px 50px 20px 30px;
       > div {
         display: flex;
         justify-content: space-between;
